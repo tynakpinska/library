c23700b310ea42780745a2eda38da9b152bbf837
tynakpinska <m.k.kepinska@gmail.com>
2023-06-18 21:19:59 +0200

    add more inputs, reset inputs after submission

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const submitButton = document.getElementById("submit-button");
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const yearInput = document.getElementById("year");
+const descriptionInput = document.getElementById("description");
 
 let myLibrary = [];
 
-function Book(title, author) {
-    this.title = title;
-    this.author = author;
+function Book(title, author, pages, year, description) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.year = year;
+  this.description = description;
 }
 
 function addBookToLibrary() {
-    const title = titleInput.value;
-    const author = authorInput.value;
-    const book = new Book(title, author);
-    myLibrary.push(book);
-    console.log(myLibrary)
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const year = yearInput.value;
+  const description = descriptionInput.value;
+  const book = new Book(title, author, pages, year, description);
+  myLibrary.push(book);
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  yearInput.value = "";
+  descriptionInput.value = "";
+  console.log(myLibrary);
 }
 
 submitButton.addEventListener("click", e => {
