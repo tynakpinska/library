f6a614fa0864d3b1c05abb7490713f08866acc66
tynakpinska <m.k.kepinska@gmail.com>
2023-07-02 11:56:50 +0200

    display book card, add buttons

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+const form = document.querySelector("form");
 const submitButton = document.getElementById("submit-button");
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
 const pagesInput = document.getElementById("pages");
 const yearInput = document.getElementById("year");
 const descriptionInput = document.getElementById("description");
+const booksList = document.querySelector(".books-list");
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "Sample book",
+    author: "Who knows",
+    pages: 234,
+    year: "1992",
+    description: "Great book",
+  },
+];
 
-function Book(title, author, pages, year, description) {
+generateBooksList();
+
+function Book(title, author, pages, year, description, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.year = year;
   this.description = description;
+  this.id = id;
 }
 
 function addBookToLibrary() {
@@ -21,14 +34,11 @@ function addBookToLibrary() {
   const pages = pagesInput.value;
   const year = yearInput.value;
   const description = descriptionInput.value;
-  const book = new Book(title, author, pages, year, description);
+  const id = myLibrary.length + 1;
+  const book = new Book(title, author, pages, year, description, id);
   myLibrary.push(book);
-  titleInput.value = "";
-  authorInput.value = "";
-  pagesInput.value = "";
-  yearInput.value = "";
-  descriptionInput.value = "";
-  console.log(myLibrary);
+  form.reset();
+  generateBooksList();
 }
 
 submitButton.addEventListener("click", e => {
@@ -36,4 +46,39 @@ submitButton.addEventListener("click", e => {
   addBookToLibrary();
 });
 
-console.log("test");
+function generateBooksList() {
+  myLibrary.forEach(book => {
+    const bookNode = document.createElement("div");
+    bookNode.className = "bookCard";
+
+    const bookTitle = document.createElement("p");
+    bookTitle.append(book.title);
+    bookNode.append(bookTitle);
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.append(book.author);
+    bookNode.append(bookAuthor);
+
+    const bookPages = document.createElement("p");
+    bookPages.append(book.pages);
+    bookNode.append(bookPages);
+
+    const bookYear = document.createElement("p");
+    bookYear.append(book.year);
+    bookNode.append(bookYear);
+
+    const bookDescription = document.createElement("p");
+    bookDescription.append(book.description);
+    bookNode.append(bookDescription);
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    bookNode.append(removeButton);
+
+    const statusButton = document.createElement("button");
+    statusButton.textContent = "Read";
+    bookNode.append(statusButton);
+
+    booksList.append(bookNode);
+  });
+}
